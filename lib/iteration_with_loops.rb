5a1e60d51804d787fa72a836f5f2638708e2fe18
Alexandra Radevich <alexa.radevich1@gmail.com>
2020-04-27 14:09:19 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,10 +1,10 @@
 def find_min_in_nested_arrays(arr)
 # need to find min of each inner array, not a general min variable
   newArr = []
-  
+  min = 100
   a = 0 
   while a < arr.length do 
-    min = 100
+    
     b = 0 
     
     while b < arr[a].length do 
